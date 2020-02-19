5a08e3c964c23cff034bf86063cec2c9421e96e5
Mirko <mirko.longo.1987@gmail.com>
2020-02-19 20:39:50 +0100

    Esercizio completo

--- js/main2.js
+++ js/main2.js
@@ -7,20 +7,22 @@ var giocatoreFittizio = "Computer";
 console.log(giocatoreFittizio);
 document.getElementById("pc").innerHTML = giocatoreFittizio;
 
-var tiroDadiGiocatoreUmano = Math.floor(Math.random() * 10) + 1;
+var tiroDadiGiocatoreUmano = Math.floor(Math.random() * 6) + 1; //Flor paviemento + 1 xk parte da 0
 console.log(tiroDadiGiocatoreUmano);
 document.getElementById("tiro-um").innerHTML = tiroDadiGiocatoreUmano;
 
-var tiroDadiGiocatoreFittizio = Math.floor(Math.random() * 10) + 1;
+var tiroDadiGiocatoreFittizio = Math.floor(Math.random() * 6) + 1;
 console.log(tiroDadiGiocatoreFittizio);
 document.getElementById("tiro-pc").innerHTML = tiroDadiGiocatoreFittizio;
 
 
 if (tiroDadiGiocatoreUmano > tiroDadiGiocatoreFittizio) {
     console.log("giocatore Umano WIN");
-    document.getElementById("risultato").innerHTML = tiroDadiGiocatoreFittizio;
-} elseif (tiroDadiGiocatoreUmano > tiroDadiGiocatoreFittizio) {
+    document.getElementById("risultatoUno").setAttribute("class","visible");
+} else if (tiroDadiGiocatoreUmano < tiroDadiGiocatoreFittizio) {
     console.log("giocatore Fittizio WIN");
+    document.getElementById("risultatoDue").setAttribute("class","visible");
 } else {
     console.log("Drow");
+    document.getElementById("risultatoTre").setAttribute("class","visible");
 }
